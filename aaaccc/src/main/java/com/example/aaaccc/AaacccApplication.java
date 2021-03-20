5ae180fb6ebbe907c0733d9dba851495fd565abf
igakki <1944551267@QQ.COM>
2021-03-20 10:33:55 +0800

    11

---
+++ aaaccc/src/main/java/com/example/aaaccc/AaacccApplication.java
@@ -0,0 +1,13 @@
+package com.example.aaaccc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaacccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaacccApplication.class, args);
+	}
+
+}
---
+++ aaaccc/src/test/java/com/example/aaaccc/AaacccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aaaccc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaacccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
